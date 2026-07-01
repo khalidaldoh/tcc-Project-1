cb4b89c9ee4a464bc9f5c644ea4e7dd8d900ba56
khalidaldoh <khalidaldoh1@gmail.com>
2026-07-01 10:16:33 +0300

    feat(database): Initial migration and model definitions
    - Added initial Alembic migration to create 'logs' and 'results' tables with appropriate columns and constraints.
    - Implemented the 'Logs' and 'Results' models in SQLAlchemy to represent the database schema.
    - Created a connection module to establish a database connection using SQLAlchemy and dotenv for environment variable management.
    
    - Developed an ingestion script to load data from a CSV file into the 'logs' table, including error handling for database operations.

---
+++ backend/app/database/models.py
@@ -0,0 +1,74 @@
+from sqlalchemy import JSON, Column, Integer,Float, String, Boolean, DateTime, BigInteger
+from .connection import Base
+class Logs(Base):
+    __tablename__ = "logs"
+    id = Column(Integer, primary_key=True, index=True)
+
+    dur = Column(Float)
+    proto = Column(String)
+    service = Column(String)
+    state = Column(String)
+
+    spkts = Column(Integer)
+    dpkts = Column(Integer)
+    sbytes = Column(Integer)
+    dbytes = Column(Integer)
+
+    rate = Column(Float)
+
+    sttl = Column(Integer)
+    dttl = Column(Integer)
+
+    sload = Column(Float)
+    dload = Column(Float)
+
+    sloss = Column(Integer)
+    dloss = Column(Integer)
+
+    sinpkt = Column(Float)
+    dinpkt = Column(Float)
+
+    sjit = Column(Float)
+    djit = Column(Float)
+
+    swin = Column(Integer)
+    stcpb = Column(BigInteger)
+    dtcpb = Column(BigInteger)
+    dwin = Column(Integer)
+
+    tcprtt = Column(Float)
+    synack = Column(Float)
+    ackdat = Column(Float)
+
+    smean = Column(Integer)
+    dmean = Column(Integer)
+
+    trans_depth = Column(Integer)
+    response_body_len = Column(Integer)
+
+    ct_srv_src = Column(Integer)
+    ct_state_ttl = Column(Integer)
+    ct_dst_ltm = Column(Integer)
+    ct_src_dport_ltm = Column(Integer)
+    ct_dst_sport_ltm = Column(Integer)
+    ct_dst_src_ltm = Column(Integer)
+
+    is_ftp_login = Column(Integer)
+    ct_ftp_cmd = Column(Integer)
+    ct_flw_http_mthd = Column(Integer)
+
+    ct_src_ltm = Column(Integer)
+    ct_srv_dst = Column(Integer)
+
+    is_sm_ips_ports = Column(Integer)
+
+    attack_cat = Column(String)
+    label = Column(Integer)
+
+
+class Results(Base):
+    __tablename__ = "results"
+    id = Column(Integer, primary_key=True, index=True)
+    created_at = Column(DateTime)
+    input_data = Column(JSON)
+    predicted_label = Column(Boolean)
---
+++ backend/app/scripts/ingestion_script.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from app.database.connection import Session
+from app.database.models import Logs
+
+
+def load_dataset():
+    df = pd.read_csv("ml/dataset_eda/UNSW_NB15_training-set.csv")
+    df = df.drop(columns=["id"])
+    records = df.to_dict(orient="records")
+
+    session = Session()
+
+    try:
+
+        session.bulk_insert_mappings(Logs, records)
+        session.commit()
+        print(f"Inserted {len(records)} records into the database.")
+    except Exception as e:
+        session.rollback()
+        print(f"Error inserting records: {e}")
+    finally: 
+        session.close()
+
+
+if __name__ == "__main__":
+    load_dataset()
