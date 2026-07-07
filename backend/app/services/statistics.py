08ec83ae90aee76e4ef96e9cc27804be41558869
khalidaldoh <khalidaldoh1@gmail.com>
2026-07-07 16:01:49 +0300

    fix: correct model reference in get_statistics function

--- backend/app/services/statistics.py
+++ backend/app/services/statistics.py
@@ -16,13 +16,13 @@ def get_statistics(db_session):
 
 
     normal = (
-        db_session.query(func.count(Results.id))
-        .filter(Results.predicted_label == False)
+        db_session.query(func.count(PredictionResults.id))
+        .filter(PredictionResults.predicted_label == False)
         .scalar()
     )
     attack = (
-        db_session.query(func.count(Results.id))
-        .filter(Results.predicted_label == True)
+        db_session.query(func.count(PredictionResults.id))
+        .filter(PredictionResults.predicted_label == True)
         .scalar()
     )
     total = normal + attack
