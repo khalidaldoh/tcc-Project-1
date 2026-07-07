1af8c5c64cac78f9d19d0d68e9329e9558514f1d
khalidaldoh <khalidaldoh1@gmail.com>
2026-07-07 17:54:23 +0300

    fix: remove unnecessary blank line in migration script

--- backend/alembic/versions/f22b4e207914_changed_some_dtypes.py
+++ backend/alembic/versions/f22b4e207914_changed_some_dtypes.py
@@ -10,7 +10,6 @@ from typing import Sequence, Union
 from alembic import op
 import sqlalchemy as sa
 
-
 # revision identifiers, used by Alembic.
 revision: str = 'f22b4e207914'
 down_revision: Union[str, Sequence[str], None] = 'dda7c246c097'
