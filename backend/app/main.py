535659a5e7bac6e1800f4adb6ac0473da2259a51
khalidaldoh <khalidaldoh1@gmail.com>
2026-07-07 18:20:04 +0300

    fix: correct method name for input_data in queued_predict_batch function

--- backend/app/main.py
+++ backend/app/main.py
@@ -94,7 +94,7 @@ def queued_predict_batch(input_data_list: list[InputDataSchema],db: Session=Depe
     for item in input_data_list:
         records.append(
             PredictionResults(
-                input_data=item.model.dumbs(),
+                input_data=item.model_dumb(),
                 status="queued"
             )
         )
